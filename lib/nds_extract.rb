81035ccc76ecbd16156ea33e8a99931bacb45c62
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-06 20:25:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -102,7 +102,7 @@ def movies_with_directors_set(source)
   result = []
   director_index = 0 
   while director_index < source.size
-    result[director_index] << movies_with_director_key(source[director_index][:name],source[director_index][:movies])         
+    result << movies_with_director_key(source[director_index][:name],source[director_index][:movies])         
     director_index += 1
   end
   binding.pry
